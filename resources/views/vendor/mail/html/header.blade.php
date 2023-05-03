4a6122bbc6b6a48115e3a314d2fddb0ed74731fa
Anthony Eduardo <105107191+AnthonyEduardo5@users.noreply.github.com>
2023-05-03 11:30:36 -0300

    Mudança no fluxo de resetar a senha

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Controle de Tarefas')
-<img src="http://localhost:8000/img/logo.png" class="logo" alt="Laravel Logo">
+<img src="http://localhost:8000/img/logo.png" class="logo" alt="Controle de Tarefas">
 @else
 {{ $slot }}
 @endif
