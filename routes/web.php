939c389da268da6b39af2d17af1083538a8969b9
Anthony Eduardo <105107191+AnthonyEduardo5@users.noreply.github.com>
2023-05-03 12:10:39 -0300

    Implementação de verificação de e-mail

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified');
+Route::resource('tarefa', 'App\Http\Controllers\TarefaController')->middleware('verified');
+
 Route::get('/mensagem-teste', function() {
     return new MensagemTesteMail();
     /* Mail::to('educardobarros06@gmail.com')->send(new MensagemTesteMail());
