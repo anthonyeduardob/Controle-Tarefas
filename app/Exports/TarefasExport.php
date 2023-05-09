9945585e8385f260ff8c4cbb10f099a81bd90ed5
Anthony Eduardo <105107191+AnthonyEduardo5@users.noreply.github.com>
2023-05-09 19:18:15 -0300

    Corrigindo caracteres especiais em exportações CSV e Manipulando os dados exportados linha por linha e formatando datas

--- app/Exports/TarefasExport.php
+++ app/Exports/TarefasExport.php
@@ -4,8 +4,10 @@ namespace App\Exports;
 
 use App\Models\Tarefa;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class TarefasExport implements FromCollection
+class TarefasExport implements FromCollection, WithHeadings, WithMapping
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +15,23 @@ class TarefasExport implements FromCollection
     public function collection()
     {
         return auth()->user()->tarefas()->get();
+    }
+
+    public function headings():array //declarando o tipo de retorno
+    {
+        return [
+            'ID da tarefa',
+            'Tarafa',
+            'Data limite de conclusão'
+        ];
+    }
+
+    public function map($linha):array
+    {
+        return [
+            $linha->id,
+            $linha->tarefa,
+            date('d/m/Y', strtotime($linha->data_limite_conclisao)),
+        ];
     }
 }
